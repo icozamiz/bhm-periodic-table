0082f08324e155a984ff3b8b51f9ba205861b446
Isabela Ozamiz <iozamiz@hopper.com>
2021-01-24 16:00:37 -0500

    google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import { ICategory } from "./types";
 import { useWindowSize } from "./hooks/useWindowSize";
 import { useDeviceTypes } from "./hooks/useDeviceTypes";
 import { CategoryTable } from "./components/category-table";
+import ReactGA from "react-ga";
 
 const setViewWidthAndHeight = (width: string, height: string) => {
   document.body.style.setProperty(`--vw`, width);
   document.body.style.setProperty(`--vh`, height);
 };
-
+const trackingId = "G-35ZL4R0S4N"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
   const windowSize = useWindowSize();
@@ -29,6 +31,15 @@ function App() {
   return (
     <Box className={containerClass}>
       <h1 className="bhm-title">Periodic Table of Canadian Black History</h1>
+      <Box className="bhm-description">
+        This
+        <a href="https://github.com/icozamiz/bhm-periodic-table">project</a>was
+        created using data from
+        <a href="https://www.parentsfordiversity.com/">Parents for Diversity</a>
+        in co-operation with
+        <a href="https://twitter.com/MrOzamiz">Mr. Ozamiz</a>a teacher in
+        Ottawa, ON, Canada.
+      </Box>
       {matchesDesktop ? (
         <Box className="bhm-periodic-table-container">
           <PeriodicTable
